0e0e661e07043c476d695fb595e87ef1b41ac451
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-20 18:59:53 +0100

    Added get Color to library

--- packages/hardhat/contracts/SockLayoutLibrary.sol
+++ packages/hardhat/contracts/SockLayoutLibrary.sol
@@ -4,20 +4,72 @@ pragma solidity ^0.8.0;
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
 // Defining Library
+
 library SockLayoutLibrary {
+    function GetColor(uint256 colorInt) public pure returns(string memory){ 
+       string memory color ; 
+       if(colorInt == 0 ) {
+            // RED
+           color = "#EB4545";
+       }
+       else if (colorInt == 1) {
+            // Pink
+            color = "#FDA2F4";
+       }
+       else if(colorInt == 2) {
+          //Orange
+          color ="#FF8A01";
+       } 
+       else if(colorInt == 3) {
+         //Yellow
+         color = "#FDED5E";
+       }
+       else if(colorInt == 4) {
+         //Green
+         color = "#B4E847";
+       }
+       else if(colorInt == 5) {
+         //Dark Green
+         color = "#70AE3F";
+       }
+       else if(colorInt == 6) {
+         //Blue
+         color = "#98C1FF";
+       }
+       else if(colorInt == 7) {
+         //Dark Blue
+         color = "#3E3BD3";
+       }
+       else if(colorInt == 8) {
+         //Purple
+         color = "#CF81FF";
+       }
+       else if(colorInt == 9) {
+         //Gray
+         color = "#D9D9D9";
+       }
+       else if(colorInt == 10) {
+         //White
+         color = "#FFFFFF";
+       }
+       else {
+         // Black
+         color ="#000000";
+       }
+      return color;
+     }
+
     function GetLayout(string memory towAndHeelColor , string memory cuffColor, string memory color) public pure returns(string memory){
       string memory sockLayout;
       sockLayout =  string(abi.encodePacked(
             '     <g transform="translate(-140, -100)">', 
             '         <path', 
             '             d="M785.9 275.626C725.097 247.898 669.212 239.882 661.077 257.721L611.495 366.449C635.241 404.755 712.524 478.465 831.681 466.859L881.263 358.13C889.398 340.292 846.702 303.353 785.9 275.626Z"', 
-            '             fill="white" stroke="black" stroke-width="8" />', 
+            '             fill="white"',
+            '             stroke="black" stroke-width="8" />', 
             '         <path', 
             '             d="M881.497 358.237C873.362 376.076 817.477 368.06 756.674 340.332C695.871 312.605 653.176 275.666 661.311 257.827C669.445 239.989 725.33 248.005 786.133 275.732C846.936 303.46 889.632 340.398 881.497 358.237Z"', 
             '             fill="black" />', 
-            '         <path', 
-            '             d="M881.497 358.237C873.362 376.076 817.477 368.06 756.674 340.332C695.871 312.605 653.176 275.666 661.311 257.827M881.497 358.237C889.632 340.398 846.936 303.46 786.133 275.732C725.33 248.005 669.445 239.989 661.311 257.827M881.497 358.237L831.914 466.965M661.311 257.827L611.728 366.556M256.592 577.741L474.708 564.611C504.256 562.833 530.407 544.883 542.69 517.949L611.728 366.556M256.592 577.741C230.213 582.564 175.625 607.311 168.301 667.718C159.146 743.226 231.83 760.984 243.946 768.158M256.592 577.741C289.999 612.941 334.238 700.305 243.946 768.158M243.946 768.158C253.638 773.896 471.392 785.967 604.133 791.729M604.133 791.729C736.874 797.492 735.861 677.598 735.861 677.598M604.133 791.729C618.96 685.946 698.13 671.565 735.861 677.598M735.861 677.598L831.914 466.965M611.728 366.556C635.475 404.862 712.758 478.572 831.914 466.965"', 
-            '             stroke="black" stroke-width="8" />', 
             '         <path', 
             '             d="M168.509 667.263C175.832 606.857 230.421 582.109 256.8 577.286C290.206 612.486 334.446 699.85 244.153 767.703C232.038 760.529 159.354 742.771 168.509 667.263Z"', 
             '             fill="white" />', 
