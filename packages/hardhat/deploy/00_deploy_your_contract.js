f2968d183823f9db107bb04e8980b7639c3ccfa9
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-21 18:53:23 +0100

    Deploy Script

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -9,141 +9,60 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
 
-  // await deploy("EyeLibrary", {
-  //   // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   from: deployer,
-  //   // args: [ "Hello", ethers.utils.parseEther("1.5") ],
-  //   log: true,
-  //   waitConfirmations: 5,
-  // });
+  await deploy("SockPinLibrary", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  const SockPinLibrary = await ethers.getContract("SockPinLibrary", deployer);
+
+  await deploy("SockLayoutLibrary", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  const SockLayoutLibrary = await ethers.getContract(
+    "SockLayoutLibrary",
+    deployer
+  );
 
   await deploy("StyleLibrary", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
     waitConfirmations: 5,
   });
 
-  await deploy("BodyLibrary", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-    from: deployer,
-    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
-    log: true,
-    waitConfirmations: 5,
-  });
-
-  // const EyeLibrary = await ethers.getContract("EyeLibrary", deployer);
   const StyleLibrary = await ethers.getContract("StyleLibrary", deployer);
-  const BodyLibrary = await ethers.getContract("BodyLibrary", deployer);
 
-  // const Eye = await deploy("Eye", {
-  //   // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   from: deployer,
-  //   // args: [ "Hello", ethers.utils.parseEther("1.5") ],
-  //   log: true,
-  //   waitConfirmations: 5,
-  //   libraries: {
-  //     EyeLibrary: EyeLibrary.address,
-  //   },
-  // });
-
-  await deploy("YourCollectible", {
+  await deploy("ThisSocks", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     // args: [],
     log: true,
     waitConfirmations: 5,
     libraries: {
+      SockLayoutLibrary: SockLayoutLibrary.address,
+      SockPinLibrary: SockPinLibrary.address,
       StyleLibrary: StyleLibrary.address,
-      BodyLibrary: BodyLibrary.address,
     },
   });
 
-  // Getting a previously deployed contract
-  const YourCollectible = await ethers.getContract("YourCollectible", deployer);
-  // await YourCollectible.setPurpose("Hello");
-
-  // await YourCollectible.addNft(Eye.address);
-
-  // // To take ownership of YourCollectible using the ownable library uncomment next line and add the
-  // // address you want to be the owner.
-
-  // await YourCollectible.transferOwnership(
-  //   "0x54179E1770a780F2F541f23CB21252De12977d3c"
-  // );
-
-  /*
-    To take ownership of yourContract using the ownable library uncomment next line and add the 
-    address you want to be the owner. 
-    // yourContract.transferOwnership(YOUR_ADDRESS_HERE);
-
-    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
-
-  /*
-  //If you want to send value to an address from the deployer
-  const deployerWallet = ethers.provider.getSigner()
-  await deployerWallet.sendTransaction({
-    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-    value: ethers.utils.parseEther("0.001")
-  })
-  */
-
-  /*
-  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  const yourContract = await deploy("YourContract", [], {
-  value: ethers.utils.parseEther("0.05")
-  });
-  */
-
-  /*
-  //If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  const yourContract = await deploy("YourContract", [], {}, {
-   LibraryName: **LibraryAddress**
-  });
-  */
-
-  // Verify your contracts with Etherscan
-  // You don't want to verify on localhost
-  // try {
-  //   if (chainId !== localChainId) {
-  //     await run("verify:verify", {
-  //       address: EyeLibrary.address,
-  //       contractArguments: [],
-  //     });
-  //   }
-  // } catch (err) {
-  //   if (err.message.includes("Reason: Already Verified")) {
-  //     console.log("Contract is already verified!");
-  //   }
-  // }
-
-  // try {
-  //   if (chainId !== localChainId) {
-  //     await run("verify:verify", {
-  //       address: Eye.address,
-  //       contractArguments: [],
-  //       libraries: {
-  //         EyeLibrary: EyeLibrary.address,
-  //       },
-  //     });
-  //   }
-  // } catch (err) {
-  //   if (err.message.includes("Reason: Already Verified")) {
-  //     console.log("Contract is already verified!");
-  //   }
-  // }
+  const ThisSocks = await ethers.getContract("ThisSocks", deployer);
 
   try {
     if (chainId !== localChainId) {
       await run("verify:verify", {
-        address: YourCollectible.address,
+        address: ThisSocks.address,
         contractArguments: [],
         libraries: {
+          SockPinLibrary: SockPinLibrary.address,
+          SockLayoutLibrary: SockLayoutLibrary.address,
           StyleLibrary: StyleLibrary.address,
-          BodyLibrary: BodyLibrary.address,
         },
       });
     }
@@ -154,7 +73,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   }
 };
 module.exports.tags = [
-  // "EyeLibrary",
+  // "SockPinLibrary",
   // "Eye",
-  "YourCollectible",
+  "ThisSock",
 ];
