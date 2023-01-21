03a5b1d604da259c6ddd3007565b28ad697c6031
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-21 16:54:43 +0100

    Sock Backgrounds

---
+++ packages/hardhat/contracts/SockBackgroundLibrary.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+
+library SockBackgroundLibrary {
+     function GetBackgroundColor(uint256 colorInt) public pure returns(string memory){ 
+       string memory color ;
+       if(colorInt == 0 ) {
+            // WHITE
+           color = "#F5F5F5";
+       }
+       else if (colorInt == 1) {
+            // GRAY
+            color = "#DCDCDC";
+       }
+       else if(colorInt == 2) {
+          //Orange
+          color ="#FDB0B0";
+       } 
+       else if(colorInt == 3) {
+         //Pink
+         color = "#FFD6F1";
+       }
+       else if(colorInt == 4) {
+         //YELLOW
+         color = "#FCDFB4";
+       }
+       else if(colorInt == 5) {
+         //BRIGHT YELLOW
+         color = "#FDFFBC";
+       }
+       else if(colorInt == 6) {
+         //Green
+         color = "#A5F7C6";
+       }
+       else if(colorInt == 7) {
+         //Blue
+         color = "#D6E4FF";
+       }
+       else if(colorInt == 8) {
+         //Dark Blue
+         color = "#A5BCF7";
+       }
+       else if(colorInt == 9) {
+         //Dark PinK
+         color = "#E8C2FF";
+       }
+       else {
+         // Black
+         color ="#282828";
+       }
+      return color;
+     }
+
+} 
