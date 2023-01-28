8279475e0f0c5199ef946d9c19ed3429275a2a37
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-29 00:28:17 +0100

    Deploy Script  Patterns

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -9,6 +9,119 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
 
+  // deploy Patterns
+  await deploy("BonePattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("ChilliPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("CloudPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("FirePattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("HeartBreakPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("HeartPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("LemonPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("LightingPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("MultiplePattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("StarPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("StripePattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  await deploy("UfoPattern", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  const BonePattern = await ethers.getContract("BonePattern", deployer);
+  const ChilliPattern = await ethers.getContract("ChilliPattern", deployer);
+  const CloudPattern = await ethers.getContract("CloudPattern", deployer);
+  const FirePattern = await ethers.getContract("FirePattern", deployer);
+  const HeartBreakPattern = await ethers.getContract(
+    "HeartBreakPattern",
+    deployer
+  );
+  const HeartPattern = await ethers.getContract("HeartPattern", deployer);
+  const LemonPattern = await ethers.getContract("LemonPattern", deployer);
+  const LightingPattern = await ethers.getContract("LightingPattern", deployer);
+  const MultiplePattern = await ethers.getContract("MultiplePattern", deployer);
+  const StarPattern = await ethers.getContract("StarPattern", deployer);
+  const StripePattern = await ethers.getContract("StripePattern", deployer);
+  const UfoPattern = await ethers.getContract("UfoPattern", deployer);
+
+  await deploy("PatternLibrary", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+    libraries: {
+      BonePattern: BonePattern.address,
+      ChilliPattern: ChilliPattern.address,
+      CloudPattern: CloudPattern.address,
+      FirePattern: FirePattern.address,
+      HeartBreakPattern: HeartBreakPattern.address,
+      HeartPattern: HeartPattern.address,
+      LemonPattern: LemonPattern.address,
+      LightingPattern: LightingPattern.address,
+      MultiplePattern: MultiplePattern.address,
+      StarPattern: StarPattern.address,
+      StripePattern: StripePattern.address,
+      UfoPattern: UfoPattern.address,
+    },
+  });
+
+  const PatternLibrary = await ethers.getContract("PatternLibrary", deployer);
+
+  await deploy("PatternMetadataLibrary", {
+    from: deployer,
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  const PatternMetadataLibrary = await ethers.getContract(
+    "PatternMetadataLibrary",
+    deployer
+  );
+
+  // Pin Library
   await deploy("PinLibrary", {
     from: deployer,
     log: true,
@@ -81,6 +194,8 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
       PinLibrary: PinLibrary.address,
       PinMetadataLibrary: PinMetadataLibrary.address,
       StyleLibrary: StyleLibrary.address,
+      PatternLibrary: PatternLibrary.address,
+      PatternMetadataLibrary: PatternMetadataLibrary.address,
     },
   });
 
@@ -92,6 +207,20 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         address: ThisSocks.address,
         contractArguments: [],
         libraries: {
+          BonePattern: BonePattern.address,
+          ChilliPattern: ChilliPattern.address,
+          CloudPattern: CloudPattern.address,
+          FirePattern: FirePattern.address,
+          HeartBreakPattern: HeartBreakPattern.address,
+          HeartPattern: HeartPattern.address,
+          LemonPattern: LemonPattern.address,
+          LightingPattern: LightingPattern.address,
+          MultiplePattern: MultiplePattern.address,
+          StarPattern: StarPattern.address,
+          StripePattern: StripePattern.address,
+          UfoPattern: UfoPattern.address,
+          PatternLibrary: PatternLibrary.address,
+          PatternMetadataLibrary: PatternMetadataLibrary.address,
           SockLayoutLibrary: SockLayoutLibrary.address,
           BackgroundLibrary: BackgroundLibrary.address,
           BackgroundMetadataLibrary: BackgroundMetadataLibrary.address,
