509f127eaaa5a66ffb9857168d19944f10d8660a
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-29 00:22:48 +0100

    Patterns Matadata

---
+++ packages/hardhat/contracts/SockPattern/PatternMetadataLibrary.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+
+import "./Patterns/BonePattern.sol";
+import "./Patterns/ChilliPattern.sol";
+import "./Patterns/CloudPattern.sol";
+import "./Patterns/FirePattern.sol";
+import "./Patterns/HeartBreakPattern.sol";
+import "./Patterns/HeartPattern.sol";
+import "./Patterns/LemonPattern.sol";
+import "./Patterns/LightingPattern.sol";
+import "./Patterns/MultiplePattern.sol";
+import "./Patterns/StarPattern.sol";
+import "./Patterns/StripePattern.sol";
+import "./Patterns/UfoPattern.sol";
+
+library PatternMetadataLibrary {
+
+    function GetPatternMetadata(uint256 currentPattern) public pure returns(string memory){
+      if (currentPattern == 0) {
+         return '{"trait_type":"Pattern", "value":"Bones"}';
+      }
+      else if (currentPattern == 1) {
+         return '{"trait_type":"Pattern", "value":"Chilli Paper"}'; 
+      }
+      else if (currentPattern == 2) {
+         return '{"trait_type":"Pattern", "value":"Clouds"}';
+      }
+      else if(currentPattern == 3) {
+        return '{"trait_type":"Pattern", "value":"Fire"}'; 
+      }
+      else if(currentPattern == 4){
+         return '{"trait_type":"Pattern", "value":"HeartBreaks"}'; 
+      }
+      else if(currentPattern == 5) {
+          return '{"trait_type":"Pattern", "value":"Hearts"}'; 
+      }
+      else if(currentPattern == 6){
+         return '{"trait_type":"Pattern", "value":"Lemons"}'; 
+      }
+      else if(currentPattern == 7){
+        return '{"trait_type":"Pattern", "value":"Lighting"}'; 
+      }
+      else if(currentPattern == 8){
+        return '{"trait_type":"Pattern", "value":"Rainbows"}'; 
+      }
+      else if(currentPattern == 9) {
+        return '{"trait_type":"Pattern", "value":"Stars"}'; 
+      }
+      else if((currentPattern == 10)) {
+        return '{"trait_type":"Pattern", "value":"Stripes"}'; 
+      }
+      else if(currentPattern == 11){
+        return '{"trait_type":"Pattern", "value":"Ufos"}'; 
+      }
+      else {
+         return "";
+      }
+    }
+}
