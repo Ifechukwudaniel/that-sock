ae891c6a2159643d5bc900e34edd7d7549f1ada4
Angry Coding <mailidanielifechukwu@gmail.com>
2023-01-30 20:00:39 +0100

    removed useless files

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,5 +1,1 @@
 export { default as Home } from "./Home";
-export { default as Preview } from "./Preview";
-export { default as ExampleUI } from "./ExampleUI";
-export { default as Hints } from "./Hints";
-export { default as Subgraph } from "./Subgraph";
