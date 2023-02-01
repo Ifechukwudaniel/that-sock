7cdc8ec621156e6aaf366020b79294da3cd1f7f1
Angry Coding <mailidanielifechukwu@gmail.com>
2023-02-01 23:00:19 +0100

    fixed rpc error

--- packages/react-app/src/helpers/Web3ModalSetup.js
+++ packages/react-app/src/helpers/Web3ModalSetup.js
@@ -22,7 +22,8 @@ const web3ModalSetup = () =>
   new Web3Modal({
     network: "mainnet", // Optional. If using WalletConnect on xDai, change network to "xdai" and add RPC info below for xDai chain.
     cacheProvider: true, // optional
-    theme: "light", // optional. Change to "dark" for a dark theme.
+    theme: "light",
+     // optional. Change to "dark" for a dark theme.
     providerOptions: {
       walletconnect: {
         package: WalletConnectProvider, // required
