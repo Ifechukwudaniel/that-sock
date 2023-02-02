40dd92fe56bad79c5e160868fc85fc2c32d17781
Angry Coding <mailidanielifechukwu@gmail.com>
2023-02-02 10:11:01 +0100

    removed icons and changed images

--- packages/react-app/src/components/Icons/SocksAndPeg.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BackgroundImageComponent = ({ src, width, height }) => {
-  return <img src={src} width={width} height={height} alt="socks" />;
-};
-
-export default BackgroundImageComponent;
