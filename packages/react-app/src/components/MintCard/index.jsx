8fb902ece4b2ad7b919944614b76a3921b196e08
Angry Coding <mailidanielifechukwu@gmail.com>
2023-02-03 08:43:29 +0100

    almost done with mint card

--- packages/react-app/src/components/MintCard/index.jsx
+++ packages/react-app/src/components/MintCard/index.jsx
@@ -3,35 +3,35 @@ import { Button } from "antd";
 import React from "react";
 import Party from "../Icons/Party";
 import "./MintCard.css";
+import ScratchSockIcon from "../Icons/ScratchSockIcon";
 
-function MintCard() {
+function MintCard({ handleMint }) {
   return (
-    <div className="mint__card__background-1">
-      <div className="mint__card__background-2">
-        <div className="mint__card__background-3">
-          <div className="mint__card__background-4">
-            <div className="Mint__Card">
-              <div className="Card__Head">
-                <div className="party__div">
-                  <Party />
-                </div>
-                <div className="Header__Text">
-                  <strong>Scratch this protective layer to see the true colours of your Sock</strong>
-                </div>
-              </div>
-              <div className="SocksCardImage">
-                <img src={"/assets/sockScratch.png"} />
-              </div>
-              <div className="Footer__Text">
-                <b>
-                  Only <span>3728 Socks</span>available on a price curve <span>increasing 0.2% </span>with each new
-                  mint. // Double the supply of the <a href="#">Original Ethereum Mainnet That Socks!</a>
-                </b>
-              </div>
-              <div>
-                <Button className="mint__button"> Mint Now </Button>
-              </div>
+    <div className="mint-card__wrapper">
+      <div className="mint__card__background-1" />
+      <div className="mint__card__background-2" />
+      <div className="mint__card__background-3" />
+      <div className="mint__card__background-4">
+        <div className="Mint__Card">
+          <div className="Card__Head">
+            <div className="party__div">
+              <Party />
             </div>
+            <div className="Header__Text">
+              <strong>Scratch this protective layer to see the true colours of your Sock</strong>
+            </div>
+          </div>
+          <div className="SocksCardImage"></div>
+          <div className="Footer__Text">
+            <b>
+              Only <span>3728 Socks</span>available on a price curve <span>increasing 0.2% </span>with each new mint. //
+              Double the supply of the <a href="#">Original Ethereum Mainnet That Socks!</a>
+            </b>
+          </div>
+          <div>
+            <Button onClick={handleMint} className="mint__button">
+              Mint Now
+            </Button>
           </div>
         </div>
       </div>
